01bd358297e2fb5c00e5bb21e73f4ec912b65b60
Nguyen Thanh Tung <tung140520@gmail.com>
2021-04-01 18:36:00 +0700

    user

--- app/Http/Controllers/admin/UserController.php
+++ app/Http/Controllers/admin/UserController.php
@@ -85,9 +85,11 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Request $rq)
     {
-        //
+        $uid = base64_decode();
+
+        return $this->_user->desstroy();
     }
 
     public function upgrade(Request $rq)
